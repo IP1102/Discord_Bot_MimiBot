ff530600b97f9b617bc31a3bc0ad89baebf9f0a6
IP1102 <indranil1102@gmail.com>
2021-04-08 03:30:29 +0530

    Added Gifs

--- mimi.js
+++ mimi.js
@@ -1,10 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const express = require('express')
+const axios = require('axios')
+const bodyParser = require('body-parser')
 
 const app = express()
 
 require('dotenv').config()
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
 
 client.login(process.env.BOT_TOKEN)
 
@@ -30,14 +34,29 @@ var appreciationArray=[
 ]
 
 client.on('message',(msg)=>{
-    if (msg.content.toLowerCase().includes('bublee')) {
+    if (msg.content.toLowerCase().includes('bublee')||msg.content.toLowerCase().includes('saronee')) {
+
         index = Math.floor(Math.random()*appreciationArray.length)
-        msg.channel.send(`Saronee is ${appreciationArray[index]}!`)
+        msg.channel.send(`Saro is ${appreciationArray[index]}!`)
+        axios.get(`https://g.tenor.com/v1/search?q=love&key=${process.env.TRENOR_API}&limit=20`)
+        .then(resp=>{
+            indexGif = Math.floor(Math.random()*resp.data.results.length)
+            msg.channel.send(resp.data.results[indexGif].url);
+        })
+
+
     }
 
-    if (msg.content.toLowerCase().includes('jess_ica')) {
+    if (msg.content.toLowerCase().includes('jess_ica')||msg.content.toLowerCase().includes('jessica')) {
+
         index = Math.floor(Math.random()*abuseArray.length)
-        msg.channel.send(`Jessica is ${abuseArray[index]}!`)
+        msg.channel.send(`Jess is ${abuseArray[index]}!`)
+
+        axios.get(`https://g.tenor.com/v1/search?q=hate&key=${process.env.TRENOR_API}&limit=20`)
+        .then(resp=>{
+            indexGif = Math.floor(Math.random()*resp.data.results.length)
+            msg.channel.send(resp.data.results[indexGif].url);
+        })
     }
 })
 
